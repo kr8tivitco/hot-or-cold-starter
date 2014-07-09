7334e4e4ff2d9344ada747c4271cb1b6125f5ec8
kr8tivitco <knmckinney@Kristons-MacBook-Pro.local>
2014-07-09 14:07:45 -0400

    add functions commit
    Added random number generate function
    Added user guess function hot/cold

--- js/app.js
+++ js/app.js
@@ -1,20 +1,93 @@
 
 $(document).ready(function(){
 
+	var guessCount =0;
+	var guess =0;
+	var userGuess =0;
+	var randNum =0;
 	
+
+		$('#guessButton').click(function() {
 	
-	
-	$('#guessButton').click(function() {
+	//$('#guessButton').click(playGame);
 		
-		var guess = $('#userGuess').val();
-		var userGuess = +guess;
-		var guessCount = $("#count").text(parseInt($("#count").text()) + 1);
-		//alert(userGuess + '' + guessCount);
+		//var	playGame = function() {		
+
+			guess = $('#userGuess').val();
+			userGuess = +guess;
+			//var guessCount =0; // $("#count").text(parseInt($("#count").text()) + 1);
+		
+			guessCount++;	
+			alert("User Guess Is: " + userGuess + "  Guess Count Is: " + guessCount);
+		
+			userGuessFunc(userGuess, randNum);
+			alert("userGuessFunc called");
+
+			return false; // stops page from automatically reloading when #guessButton 
+						//selected. Use the below link  as a referene
+						//http://stackoverflow.com/questions/1878264/how-do-i-make-an-html-button-not-reload-the-page
+		});
+
+	//playGame();
+
+	/*var newGame = function () {
+
+		$('.new').click(function() {
+			guessCount = 0;
+			playGame();
+		}
+	}*/
+	
+	
+////////////////randNumFunc generates random number between 1-100////////////////////
+	var randNumFunc =function () {//randomIntFromInterval()
+	
+    	var minNum  = 1;
+    	var maxNum = 100;
+
+    	randNum = Math.floor(Math.random()*(maxNum-minNum+1)+minNum);
+    return randNum;
+	}
+/////////////////////////////////////////////////////////////////////////////////////
+	
+	randNumFunc();
 
 
-	});
+////////////////userGuessFunc determines if user is Hot|| Cold /////////////////////
+	var userGuessFunc =function (arg1, arg2) {
+		alert('rand number is: ' + randNum);
+		var guessCalc = +(Math.abs(arg2 - arg1));
+		//alert('guessCalc is: ' + guessCalc);
 
+		if (guessCalc > 50) {
+			alert(userGuess + ' - ' + randNum + ' = ' + (userGuess - randNum));
+			$("#feedback").html("You're ICE Cold");
+		
+		} else if ((guessCalc < 50) && (guessCalc > 30)) {
+			$("#feedback").html("You're Cold");
 
+		} else if ((guessCalc < 30) && (guessCalc > 20)) {
+			$("#feedback").html("You're Warm");
+
+		} else if ((guessCalc < 20) && (guessCalc > 10)) {
+			$("#feedback").html("You're Hot");
+
+		} else if ((guessCalc < 10) && (guessCalc > 5)) {
+			$("#feedback").html("You're VERY Hot");
+
+		} else if ((guessCalc < 5) && (guessCalc > 1)) {
+			$("#feedback").html("You're BOILING HOT");
+
+		} else {
+			$("#feedback").html("You have guessed it!!! " + arg1 + " = " + arg2);
+		}
+
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////
+	
+
+/////////////////////////////////////////////////////////////////////////////////////
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -28,10 +101,5 @@ $(document).ready(function(){
 
 
 
-
-
-	//var newGame = function() {
-
-
-	//}	
 });
+
